69325b8a50fc44203eea573cecad7d8e59c9fcca
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-03-04 21:52:35 +0200

    adding getData function

--- utils.js
+++ utils.js
@@ -1,18 +1,29 @@
 const fs = require("fs");
 const fsPromise = require("fs/promises");
 const shortid = require("shortid");
+let dataBase;
+process.env.NODE_ENV === "test"
+  ? (dataBase = "testDataBase")
+  : (dataBase = "dataBase");
 
 class DataBase {
   constructor() {
     this.urls = [];
+    getData().then((data) => {
+      if (data) {
+        this.urls = data;
+      }
+    });
   }
   saveUrl(fullURL) {
     const newUrl = new Url(fullURL);
     this.urls.push(newUrl);
     const data = JSON.stringify(this.urls, null, 4);
-    return fsPromise.writeFile("./DataBase/dataBase.json", data).then((res) => {
-      return newUrl;
-    });
+    return fsPromise
+      .writeFile(`./DataBase/${dataBase}.json`, data)
+      .then((res) => {
+        return newUrl;
+      });
   }
   checkExistenceFullUrl(Url) {
     return compareUrl(Url, "fullUrl");
@@ -34,7 +45,7 @@ class DataBase {
       })
       .then((data) => {
         data = JSON.stringify(data, null, 4);
-        fsPromise.writeFile("./DataBase/dataBase.json", data, (e) => {});
+        fsPromise.writeFile(`./DataBase/${dataBase}.json`, data, (e) => {});
       });
   }
 }
@@ -48,7 +59,7 @@ class Url {
 }
 function compareUrl(url, kind) {
   return fsPromise
-    .readFile("./DataBase/dataBase.json")
+    .readFile(`./DataBase/${dataBase}.json`)
     .then((res) => {
       let data = JSON.parse(res);
       let currentUrl = data.find((urlObj) => {
@@ -59,10 +70,22 @@ function compareUrl(url, kind) {
       if (currentUrl) {
         return currentUrl;
       }
-      throw new Error();
+      return new Error();
     })
     .catch((e) => {
       return;
+    });
+}
+function getData() {
+  return fsPromise
+    .readFile(`./database/${dataBase}.json`, "utf8")
+    .then((data) => {
+      const parseData = JSON.parse(data);
+      return parseData;
+    })
+    .catch((e) => {
+      console.error("Can't read the data base");
+      return false;
     });
 }
 
