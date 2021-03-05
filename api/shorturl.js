8bf07ae03570b74049d57352a4a0e19aaecfb04a
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-03-05 17:01:02 +0200

    updating existance function

--- api/shorturl.js
+++ api/shorturl.js
@@ -2,18 +2,19 @@ const express = require("express");
 const fsPromise = require("fs/promises");
 const DataBase = require("../utils");
 const validUrl = require("valid-url");
-const shortid = require("shortid");
 const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded());
 const DB = new DataBase();
 
-router.post("/new", checkExistence, (request, response) => {
+router.post("/new", checkIfExists, (request, response) => {
+  console.log(DB);
   const url = request.body.url;
   if (!validUrl.isUri(url)) {
-    response.status(404).json({ msg: new Error("Invalid URL") });
+    response.status(404).json({ msg: `${new Error()}"Invalid URL` });
     return;
   }
+
   DB.saveUrl(url)
     .then((newUrl) => {
       response
@@ -21,13 +22,13 @@ router.post("/new", checkExistence, (request, response) => {
         .json(` original_url: ${newUrl.fullUrl}, short_url: ${newUrl.shortid}`);
     })
     .catch((e) => {
-      response.status(500).send(`${error}`);
+      response.status(500).send(`${e}`);
     });
 });
 
 router.get("/:shorturl", (request, response) => {
   const shortUrl = request.params.shorturl;
-  DB.checkExistenceShortid(shortUrl, "shortid")
+  DB.checkExistence(shortUrl, "shortid")
     .then((urlObj) => {
       if (!urlObj) {
         response.status(404).json({ msg: new Error("There is no short URL ") });
@@ -42,20 +43,21 @@ router.get("/:shorturl", (request, response) => {
     });
 });
 
-function checkExistence(request, response, next) {
+function checkIfExists(request, response, next) {
   const url = request.body.url;
-  DB.checkExistenceShortid(url, "shortid")
+  DB.checkExistence(url, "fullUrl")
     .then((data) => {
       if (data) {
         response.status(200).json(data);
         return;
       }
 
-      return new Error();
+      throw new Error();
     })
     .catch((e) => {
       next();
     });
 }
+console.log(DB.urls);
 
 module.exports = { router, DB };
--- utils.js
+++ utils.js
@@ -9,10 +9,8 @@ process.env.NODE_ENV === "test"
 class DataBase {
   constructor() {
     this.urls = [];
-    getData().then((data) => {
-      if (data) {
-        this.urls = data;
-      }
+    getData().then((res) => {
+      this.urls = res;
     });
   }
   saveUrl(fullURL) {
@@ -21,15 +19,29 @@ class DataBase {
     const data = JSON.stringify(this.urls, null, 4);
     return fsPromise
       .writeFile(`./DataBase/${dataBase}.json`, data)
-      .then((res) => {
+      .then((response) => {
         return newUrl;
       });
   }
-  checkExistenceFullUrl(Url) {
-    return compareUrl(Url, "fullUrl");
-  }
-  checkExistenceShortid(shortUrl) {
-    return compareUrl(shortUrl, "shortid");
+
+  checkExistence(url, kind) {
+    return fsPromise
+      .readFile(`./DataBase/${dataBase}.json`)
+      .then((res) => {
+        let data = JSON.parse(res);
+        let currentUrl = data.find((urlObj) => {
+          if (urlObj[kind] === url) {
+            return true;
+          }
+        });
+        if (currentUrl) {
+          return currentUrl;
+        }
+        throw new Error();
+      })
+      .catch((e) => {
+        return;
+      });
   }
   updateRedirectClicks(short) {
     compareUrl(short, "shortid")
@@ -53,7 +65,7 @@ class Url {
   constructor(fullUrl) {
     this.fullUrl = fullUrl;
     this.shortid = shortid();
-    this.createdAt = Date.now();
+    this.createdAt = new Date().toLocaleString();
     this.clicks = 0;
   }
 }
@@ -77,16 +89,14 @@ function compareUrl(url, kind) {
     });
 }
 function getData() {
-  return fsPromise
-    .readFile(`./database/${dataBase}.json`, "utf8")
-    .then((data) => {
-      const parseData = JSON.parse(data);
-      return parseData;
-    })
-    .catch((e) => {
-      console.error("Can't read the data base");
-      return false;
-    });
+  return fsPromise.readFile(`./DataBase/${dataBase}.json`).then((res) => {
+    const parseData = JSON.parse(res);
+    return parseData;
+  });
+  //   .catch((e) => {
+  //     console.error("Can't get data from dataBase");
+  //     return false;
+  //   });
 }
 
 module.exports = DataBase;
