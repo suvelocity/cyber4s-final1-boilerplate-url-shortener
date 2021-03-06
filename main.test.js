d18ba7393ef599b92dbf9b4bbeaa729e64bcf8f0
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-03-06 17:26:09 +0200

    adding final tests

--- main.test.js
+++ main.test.js
@@ -1,23 +1,42 @@
 const request = require("supertest");
 const app = require("./app");
 const fsPromise = require("fs/promises");
-
 const { DB } = require("./api/shorturl");
-describe("POST route tests", () => {
-  const receivedUrl = { url: "https://www.youtube.com/" };
-  const expectedResponse = {
-    fullUrl: "https://www.youtube.com/",
-    shortid: "gv5TqP1iQ",
-    createdAt: "5.3.2021, 12:59:24",
-    clicks: 1,
-  };
+const { response } = require("express");
+const testsUrl = { url: "https://www.youtube.com/" };
+const expectedResponse = {
+  fullUrl: "https://www.youtube.com/",
+  shortid: "gv5TqP1iQ",
+  createdAt: "5.3.2021, 12:59:24",
+  clicks: 1,
+};
+const newUrl = { url: "https://finviz.com/" };
 
+/********resetting the tests database after each test to it`s initial value*******/
+afterAll(async () => {
+  await fsPromise.writeFile(
+    `./DataBase/testDataBase.json`,
+    JSON.stringify([expectedResponse], null, 4),
+    (e) => {
+      console.log(e);
+    }
+  );
+});
+
+describe("POST route tests", () => {
   //******************Error testing for checking response *********************/
+  it("Should save to the dataBase new given url's", async () => {
+    const DBLengthBefore = DB.urls.length;
+    const res = await request(app).post("/api/shorturl/new").send(newUrl);
+
+    expect(res.status).toBe(201);
+    expect(DB.urls.length).toBeGreaterThan(DBLengthBefore);
+  });
 
   it("Should get a full url and return the url's object", async () => {
     const response = await request(app)
       .post("/api/shorturl/new")
-      .send(receivedUrl);
+      .send(testsUrl);
     const storedUrl = DB.urls[0];
     // Is the status code 200
     expect(response.status).toBe(200);
@@ -28,7 +47,6 @@ describe("POST route tests", () => {
   //******************Error testing for sending invalid URL *********************/
   it("Should return an error message for invalid url", async () => {
     const invalidUrl = { url: "niohgfoeirjgio[rejg].com" };
-    const newUrl = { url: "https://finviz.com/" };
 
     const response = await request(app)
       .post("/api/shorturl/new")
@@ -55,5 +73,24 @@ describe("GET test", () => {
     const res = await request(app).get("/api/shorturl/-Xiuplgb8");
 
     expect(res.status).toBe(404);
+  });
+
+  test(" if clicks property increases by one on each short link redirection", async () => {
+    const res = await request(app).get("/api/shorturl/gv5TqP1iQ");
+    const thisUrl = DB.urls.find((urlObj) => {
+      if (urlObj.shortid === "gv5TqP1iQ") {
+        return true;
+      }
+    });
+    console.log(thisUrl);
+    expect(thisUrl.clicks).toBe(2);
+  });
+});
+
+describe("Statistics test", () => {
+  it("Should get a short url and send back his obj", async () => {
+    const res = await request(app).get("/api/statistics/gv5TqP1iQ");
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(expectedResponse);
   });
 });
