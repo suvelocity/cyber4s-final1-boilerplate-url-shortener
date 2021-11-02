c3f02c875a64d58e1039c5972e90714ac0a6ef49
ElayGelbart <elaygelbart@gmail.com>
2021-11-02 16:52:43 +0200

    Backend - very basic server

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -1,15 +1,26 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
 
-app.use("/public", express.static(`./public`));
+app.use("/dist", express.static(`./dist`));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "../Frontend/index.html");
 });
+
+app.post("/api/shorturl/:nameOfNewUrl", (req, res, next) => {
+
+})
+
+app.get("api/statistic/:shorturl-name", (req, res, next) => {
+  res.send(/*statics in json */)
+});
+
+app.get("/:wishUrl", (req, res, next) => {
+  res.send(/*redirct to the site*/)
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("server is on");
