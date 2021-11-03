5a393862a20b61f43cdece1ee565c8f0f5a57814
ElayGelbart <elaygelbart@gmail.com>
2021-11-03 17:44:11 +0200

    Frontend - Validation Error UI

--- src/Frontend/main.js
+++ src/Frontend/main.js
@@ -3,17 +3,17 @@
 const sendOldURLToServerWithNameOfNew = async () => {
   const oldURLvalue = document.getElementById("oldURLinput").value;
   const newURLvalue = document.getElementById("newURLinput").value;
+  if (!validator.isURL(oldURLvalue)) {
+    createErrorInputUI(document.getElementById("oldURLinput"), "Long Url Must Be Valid");
+    return
+  }
   if (!validator.isLength(newURLvalue, { min: 3, max: 15 })) {
-    alert("write something") //change this later;
+    createErrorInputUI(document.getElementById("newURLinput"), "New Url Name Must be Minimum 3 and Maximum 15");
     return
   }
   if (!validator.isAlphanumeric(newURLvalue)) {
-    alert("enter Valid New Name");
-    return 'URL MUST CONTAIN HTTP/S' //change this later
-  }
-  if (!validator.isURL(oldURLvalue)) {
-    alert("enter valid URL")
-    return 'URL MUST CONTAIN HTTP/S' //change this later
+    createErrorInputUI(document.getElementById("newURLinput"), "New Url Name Must Contain Only AlphaBet and Numbers");
+    return
   }
   const response = await axios.post(`http://localhost:8080/api/shorturl/${newURLvalue}`, { oldurl: oldURLvalue });
   console.log(response);
@@ -22,18 +22,19 @@ const sendOldURLToServerWithNameOfNew = async () => {
 const getStatisticFromURL = async () => {
   const userURLValue = document.getElementById("userCustomUrl").value;
   if (!validator.isLength(userURLValue, { min: 3, max: 15 })) {
-    alert("write something") //change this later;
+    createErrorInputUI(document.getElementById("userCustomUrl"), "New Url Name Must be Minimum 3 and Maximum 15");
     return
   }
   if (!validator.isAlphanumeric(userURLValue)) {
-    alert("enter Valid New Name");
-    return 'URL MUST CONTAIN HTTP/S' //change this later
+    createErrorInputUI(document.getElementById("userCustomUrl"), "New Url Name Must Contain Only AlphaBet and Numbers");
+    return
   }
-  const response = await axios.get(`http://localhost:8080/api/statistic/${userURLValue}`);
-  const responseUrlOBJ = response.data;
-  const divStatics = document.createElement("DIV");
-  divStatics.classList.add("regularDiv");
-  divStatics.innerHTML = `
+  try {
+    const response = await axios.get(`http://localhost:8080/api/statistic/${userURLValue}`);
+    const responseUrlOBJ = response.data;
+    const divStatics = document.createElement("DIV");
+    divStatics.classList.add("regularDiv");
+    divStatics.innerHTML = `
   <h4>${userURLValue}</h4>
   <div class="statisticParag">
   <p>Special URL Created: <span class="boldWord">${responseUrlOBJ.creationDate}</span></p>
@@ -41,8 +42,33 @@ const getStatisticFromURL = async () => {
   <p>Original URL: <a href="${responseUrlOBJ.originalUrl} target="_blank"><span class="boldWord">${responseUrlOBJ.originalUrl}</span></a></p>
   </div>
   `;
-  document.getElementById("urlStaticSect").appendChild(divStatics);
+    clearBadInput()
+    document.getElementById("urlStaticSect").appendChild(divStatics);
+  } catch (err) {
+    createErrorInputUI(document.getElementById("userCustomUrl"), "Non Existing URL");
+  }
 }
+
+const clearBadInput = () => {
+  const badInputElemArray = document.getElementsByClassName("badInput");
+  for (let value of badInputElemArray) {
+    value.classList.remove("badInput");
+  }
+  const badInputParagArray = document.getElementsByClassName("badInputPara");
+  for (let value of badInputParagArray) {
+    value.remove();
+  }
+}
+
+const createErrorInputUI = (inputElement, msg) => {
+  clearBadInput();
+  inputElement.classList.add("badInput");
+  const msgPara = document.createElement("P");
+  msgPara.classList.add("badInputPara")
+  msgPara.innerText = `${msg}`
+  inputElement.parentElement.appendChild(msgPara)
+}
+
 document.getElementById("createURLBtn").addEventListener("click", sendOldURLToServerWithNameOfNew);
 document.getElementById("getStatsticBtn").addEventListener("click", getStatisticFromURL);
 
