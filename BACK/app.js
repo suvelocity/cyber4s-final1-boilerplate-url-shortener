099fbede78daff3b65aab1b4b2d919f0b0d97dc7
shakederin <89574610+shakederin@users.noreply.github.com>
2021-11-04 16:35:41 +0200

    to heroku

--- BACK/app.js
+++ BACK/app.js
@@ -5,11 +5,11 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
 const dataBaseUse = new DataBase;
-const myURL = "http://localhost:8080"
+const myURL = "https://shakedurl.herokuapp.com/ "
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "./src")));     //able to read statics files
+app.use(express.static(path.join(__dirname, "../dist")));     //able to read statics files
 
 app.use("/", (req,res, next)=>{
   if(req. _parsedOriginalUrl.path === "/" || req. _parsedOriginalUrl.path === "/makeurl"|| req.  _parsedOriginalUrl.path === "/status"
@@ -18,7 +18,7 @@ app.use("/", (req,res, next)=>{
     try {
       const requestEnd = (req. _parsedOriginalUrl.path);
       const longURL = dataBaseUse.getLongUrlFromStorage(requestEnd);
-      res.redirect(longURL);
+      res.redirect(longURL.slice(1,longURL.length-1));
     } catch (error) {
       res.send(error);
     }
@@ -26,32 +26,32 @@ app.use("/", (req,res, next)=>{
 })
 
 app.get("/", (req,res)=>{              //homepage                             
-  res.sendFile("C:/Users/owner/Desktop/קורס CYBER4S/29.08/cyber4s-final1-boilerplate-url-shortener/Front/index.html");
+  res.sendFile("../dist/index.html");
 })
 
 app.get("/makeurl", function(req,res){
   try {
     const longUrl = JSON.stringify(req.headers.longurl);
     const customShort = req.headers.shorturl;
-    if(!customShort == undefined){
-      console.log('why am i here');
+    if(customShort){
+      console.log("in first If");
       if(dataBaseUse.isDuplicate(customShort)){
-        throw "url taken already"
+        throw "URL taken already"
       }
       else{
+        console.log("in else");
         dataBaseUse.storeUrlRelation(longUrl, customShort);
-        res.send(homeUrl + "/" + customShort)
+        res.send(myURL + "/" + customShort)
       }
     }
     
     if(dataBaseUse.isExistLong(longUrl)){
       const existingUrl = dataBaseUse.isExistLong(longUrl)
-      console.log(homeUrl + "/" + existingUrl);
-      res.send(homeUrl + "/" + existingUrl);
+      res.send(myURL + "/" + existingUrl);
     }
     else{
     const shortURL = makeId(4);
-    dataBaseUse.storeUrlRelation(longURL, shortURL);
+    dataBaseUse.storeUrlRelation(longUrl, shortURL);
     res.send(myURL + "/" + shortURL);
     }
   } catch (error) {
@@ -60,15 +60,17 @@ app.get("/makeurl", function(req,res){
 })
 
 app.get("/status", function (req,res){
-    const shortURL = req.headers.shorturl;
-    const slicedUrl = (shortURL.slice(22));
-    
-    const counter = dataBaseUse.getCounterFromStorage(slicedUrl);
-    const longurl =dataBaseUse.getLongUrlFromStorage(`/${slicedUrl}`);
-    const date =dataBaseUse.getDateFromStorage(slicedUrl);
-    const data = {counter, longurl, date};
-    console.log(data);
-    res.send(data);
+    try {
+        const shortURL = req.headers.shorturl;
+        const slicedUrl = (shortURL.slice(22));
+        const counter = dataBaseUse.getCounterFromStorage(slicedUrl);
+        const longurl =dataBaseUse.getLongUrlFromStorage(`/${slicedUrl}`);
+        const date =dataBaseUse.getDateFromStorage(slicedUrl);
+        const data = {counter, longurl, date};
+        res.send(data);
+    } catch (error) {
+      
+    }
 })
 
 function makeId(length) {
--- Front/src/index.js
+++ Front/src/index.js
@@ -4,11 +4,19 @@ const button = document.getElementById("go");
 const inputshorturl = document.getElementById("shorturl");
 const getstat = document.getElementById("getstates");
 const stats = document.getElementById("stats");
-const myAPI = "http://localhost:8080"
+const customeURL = document.getElementById("customeURL");
+const home = document.getElementById("home");
+const statsBtn = document.getElementById("statsBtn");
+const header = document.getElementById("header");
+const root = document.getElementById("root");
+
+
+
+const myAPI = "https://shakedurl.herokuapp.com/"
 
 button.addEventListener("click", async function(){
     try {
-        const shotUrl = undefined;
+        const shotUrl = customeURL.value;
         if(!validator.isURL(URLInput.value)){
             displayUrl("Please enter a valid URL");
             return
@@ -19,10 +27,9 @@ button.addEventListener("click", async function(){
                 "shorturl": shotUrl  
             }
         })
-        console.log(data);
         displayUrl(data.data);
-
-    } catch (error) {
+        URLInput.value = "";
+    } catch (error) {      
         console.log(error);
     }
 })
@@ -33,13 +40,39 @@ getstat.addEventListener("click", async function(){
             "shorturl" : inputshorturl.value 
         }
     })
+    clearStatus();
     const longurl = createElement("div", "status");
+    longurl.classList.add("showinfosmall")
     const date = createElement("div", "status");
+    date.classList.add("showinfo")
     const counter = createElement("div", "status");
+    counter.classList.add("showinfo")
     longurl.textContent = `Original URL: ${data.data.longurl}`;
     counter.textContent = `Times Visited: ${data.data.counter}`;
     date.textContent = `Date Created: ${data.data.date}`;
     stats.append(longurl, counter , date);
+})
+
+statsBtn.addEventListener("click", ()=>{
+    try {
+        URLInput.style.visibility = "hidden";
+        button.style.visibility = "hidden";
+        customeURL.style.visibility = "hidden";
+        header.style.visibility = "hidden";
+        stats.style.visibility = "visible";
+        root.style.visibility = "hidden";
+    } catch (error) {
+        
+    }
+})
+
+home.addEventListener("click", ()=>{
+    URLInput.style.visibility = "visible";
+    button.style.visibility = "visible";
+    customeURL.style.visibility = "visible";
+    header.style.visibility = "visible";
+    stats.style.visibility = "hidden";
+    root.style.visibility = "visible";
 })
 
 function displayUrl(newUrl){
@@ -51,4 +84,14 @@ function createElement(tag, className){
     const element = document.createElement(tag);
     element.classList.add(className);
     return element
+}
+
+function clearStatus(){
+    inputshorturl.value = "";
+    const status = document.getElementsByClassName('status');
+    let i = status.length-1;
+    while(i>=0){ //delete all status elemnts created
+        status[i].remove();
+        i--;
+    }
 }
