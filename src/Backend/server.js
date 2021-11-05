af5ec51faf30bf7ac2d6faa1e1816aa07ea0c603
ElayGelbart <elaygelbart@gmail.com>
2021-11-05 14:12:03 +0200

    Mobile Check - user agent check

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -7,8 +7,20 @@ app.use(cors());
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use("/", (req, res, next) => {
-  console.log("hello");
-  console.log(req.headers['user-agent']);
+  const toMatch = [
+    /Android/i,
+    /webOS/i,
+    /iPhone/i,
+    /iPad/i,
+    /iPod/i,
+    /BlackBerry/i,
+    /Windows Phone/i
+  ];
+  for (let device of toMatch) {
+    if (device.test(req.headers['user-agent'])) {
+      res.redirect("/Mobile")
+    }
+  }
   next();
 })
 
